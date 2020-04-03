471a49c90f527bf986b8cda2e45aef4963acabe8
Cefalu Wave <luke.evan.graham@gmail.com>
2020-04-02 21:11:41 -0700

    collecting date

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let cheerio = require("cheerio");
 let axios = require("axios");
+let moment = require("moment");
 
 axios.get("https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html#Table").then(function(response) {
     let $ = cheerio.load(response.data);
@@ -7,14 +8,15 @@ axios.get("https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/communit
 
     let results = [];
 
-    // let table = $("tbody").html()
-
+    let dateFull = $("tbody > tr:nth-child(1) > td > p:nth-child(3)").text().split("d ", 2);
+    let dateString = dateFull[1].split(",", 2).join()
+    let date = moment(dateString, "MMM D, YYYY").format();
     let totalPositives = $("tbody > tr:nth-child(3) > td:nth-child(2) > b").text();
     let hospitalizations = $("tbody > tr:nth-child(19) > td:nth-child(2)").html();
     let intensiveCare = $("tbody > tr:nth-child(20) > td:nth-child(2)").html();
     let deaths = $("tbody > tr:nth-child(21) > td:nth-child(2)").html();
 
-
+    console.log("Date: ", date)
     console.log("Total Positives: ", totalPositives);
     console.log("Hospitalizations: ", hospitalizations);
     console.log("Intensive Care: ", intensiveCare);
